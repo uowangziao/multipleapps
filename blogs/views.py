054ca3dc4e26b318d6ad8734f201fcf8bf99165b
ZIAO WANG <uowangziao@gmail.com>
2017-09-19 11:31:15 -0700

    first commit

---
+++ blogs/views.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+from django.shortcuts import render,HttpResponse,redirect
+
+def index(request):
+    return HttpResponse("placeholder to later display all the list of  blogs")
+def new(request):
+    return HttpResponse("place holder to display a new form to create a new blog")
+def create(request):
+    return redirect('/blogs')
+def show(request,blog_id):
+    print blog_id
+    return HttpResponse("placeholder to display blog {}".format(blog_id))
+def edit(request,blog_id):
+    return HttpResponse("placeholder to edit blog{}".format(blog_id))
+def delete(request):
+    return redirect('/blogs')
